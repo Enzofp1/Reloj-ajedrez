8752b7b94de1bfd4fafaacd58c16a89af7545ad1
Enzofp1 <130710125+Enzofp1@users.noreply.github.com>
2023-09-22 22:19:14 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ChessClock from './components/ChessClock';
 import './App.css'; 
 
@@ -6,20 +6,22 @@ function App() {
   const [initialTime, setInitialTime] = useState(600);
   const [incrementTime, setIncrementTime] = useState(5);
   const [key, setKey] = useState(0); 
-
+  const [isFinished, setIsFinished] = useState(false);
   const startNewGame = () => {
     
     setKey(key + 1); 
   };
-
+  useEffect(() => {
+    setIsFinished(false);
+  }, [key]);
   return (
     
     <div className="App">
       
-      <ChessClock key={key} initialTime={initialTime} incrementTime={incrementTime} />
+      <ChessClock setIsFinished={setIsFinished} key={key} initialTime={initialTime} incrementTime={incrementTime} />
       <div className="config-panel">
         
-        <label className='label1'>
+        {!isFinished && <> <label className='label1'>
           Tiempo a finish (segundos):
           <input
             type="number"
@@ -34,7 +36,7 @@ function App() {
             value={incrementTime}
             onChange={(e) => setIncrementTime(e.target.value)}
           />
-        </label>
+        </label></>} 
         <button onClick={startNewGame}>Nueva Partida</button>
       </div>
     </div>
--- src/components/ChessClock.jsx
+++ src/components/ChessClock.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import './ChessClock.css';
 
-const ChessClock = ({ initialTime, incrementTime }) => {
+const ChessClock = ({ initialTime, incrementTime,setIsFinished }) => {
   const [player1Time, setPlayer1Time] = useState(initialTime);
   const [player2Time, setPlayer2Time] = useState(initialTime);
   const [activePlayer, setActivePlayer] = useState(1);
@@ -9,9 +9,11 @@ const ChessClock = ({ initialTime, incrementTime }) => {
   const [alertingPlayer1, setAlertingPlayer1] = useState(false);
   const [alertingPlayer2, setAlertingPlayer2] = useState(false);
   const [winner, setWinner] = useState(null);
-
+  
   const audioRef1 = useRef(null);
   const audioRef2 = useRef(null);
+
+  const [lastClickTime, setLastClickTime] = useState(0);
 
   useEffect(() => {
     let timer;
@@ -20,8 +22,8 @@ const ChessClock = ({ initialTime, incrementTime }) => {
       timer = setInterval(() => {
         if (activePlayer === 1) {
           if (player1Time > 0) {
-            setPlayer1Time(player1Time - 1);
-            if (player1Time <= incrementTime && !alertingPlayer1) {
+            setPlayer1Time(prevTime =>Number (prevTime) - 1);
+            if (player1Time <= incrementTime && incrementTime > 0 && !alertingPlayer1) {
               setAlertingPlayer1(true);
               audioRef1.current.currentTime = 0;
               audioRef1.current.play();
@@ -29,11 +31,12 @@ const ChessClock = ({ initialTime, incrementTime }) => {
           } else {
             clearInterval(timer);
             setWinner(2);
+            setIsFinished (true);
           }
         } else {
           if (player2Time > 0) {
-            setPlayer2Time(player2Time - 1);
-            if (player2Time <= incrementTime && !alertingPlayer2) {
+            setPlayer2Time(prevTime => prevTime - 1);
+            if (player2Time <= incrementTime && incrementTime > 0 && !alertingPlayer2) {
               setAlertingPlayer2(true);
               audioRef2.current.currentTime = 0;
               audioRef2.current.play();
@@ -41,13 +44,14 @@ const ChessClock = ({ initialTime, incrementTime }) => {
           } else {
             clearInterval(timer);
             setWinner(1);
+            setIsFinished (true);
           }
         }
       }, 1000);
     } else {
       clearInterval(timer);
     }
-
+   
     return () => {
       clearInterval(timer);
     };
@@ -62,16 +66,48 @@ const ChessClock = ({ initialTime, incrementTime }) => {
   };
 
   const switchPlayer = () => {
-    setActivePlayer(activePlayer === 1 ? 2 : 1);
-    setAlertingPlayer1(false);
-    setAlertingPlayer2(false);
+    const currentTime = new Date().getTime();
+    if (
+      (isRunning || (!isRunning && player1Time > 0 && player2Time > 0)) &&
+      !winner &&
+      currentTime - lastClickTime > 300
+    ) {
+      setActivePlayer(activePlayer === 1 ? 2 : 1);
+      setAlertingPlayer1(false);
+      setAlertingPlayer2(false);
+      setLastClickTime(currentTime);
 
-    
-    if (isRunning) {
-      if (activePlayer === 1) {
-        setPlayer1Time(player1Time + incrementTime);
-      } else {
-        setPlayer2Time(player2Time + incrementTime);
+      
+      if (isRunning && incrementTime > 0) {
+        if (activePlayer === 1) {
+          setPlayer1Time(prevTime => Number (prevTime) +Number (incrementTime));
+        } else {
+          setPlayer2Time(prevTime => Number (prevTime) +Number (incrementTime));
+        }
+      }
+    }
+  };
+
+  const onClickPlayer = () => {
+
+    const currentTime = new Date().getTime();
+    if (
+      (isRunning || (!isRunning && player1Time > 0 && player2Time > 0)) &&
+      !winner &&
+      currentTime - lastClickTime > 300
+    ) {
+      setActivePlayer(activePlayer === 1 ? 2 : 1);
+      setAlertingPlayer1(false);
+      setAlertingPlayer2(false);
+      setLastClickTime(currentTime);
+
+      
+      if (isRunning && incrementTime > 0) {
+        if (activePlayer === 1) {
+          setPlayer1Time(prevTime => Number (prevTime) +Number (incrementTime));
+        } else {
+          setPlayer2Time(prevTime => Number (prevTime) +Number (incrementTime));
+        }
       }
     }
   };
@@ -86,25 +122,32 @@ const ChessClock = ({ initialTime, incrementTime }) => {
       </audio>
       {winner ? (
         <div className="winner-message">
-          Jugador {winner} gana
+          Jugador {winner} gana!
+         
         </div>
       ) : (
         <div className="time-display">
-          <div className={`player-time ${activePlayer === 1 ? 'active' : ''}`}>
+          <div
+            className={`player-time ${activePlayer === 1 ? 'active' : ''}`}
+            onClick={() => activePlayer !== 1 && onClickPlayer()}
+          >
             Jugador 1: {player1Time} segundos
           </div>
-          <div className={`player-time ${activePlayer === 2 ? 'active' : ''}`}>
+          <div
+            className={`player-time ${activePlayer === 2 ? 'active' : ''}`}
+            onClick={() => activePlayer !== 2 && onClickPlayer()}
+          >
             Jugador 2: {player2Time} segundos
           </div>
         </div>
       )}
-      <button onClick={toggleClock}>{isRunning ? 'Pausar' : 'Iniciar'}</button>
-      <button onClick={switchPlayer}>Cambiar jugador</button>
+      {!winner&&  <button onClick={toggleClock}>{isRunning ? 'Pausar' : 'Iniciar'}</button>} 
     </div>
   );
 };
 
 export default ChessClock;
+
 
 
 
